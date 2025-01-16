60c5bdef87bd1375375e378cfc1d56f33875b507
andrey <andrey@sublimestudio.es>
2025-01-16 13:08:54 +0100

    --wip-- [skip ci]

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
 import './assets/style.css'
-import {VueFire} from 'vuefire'
-import {firebaseApp} from './firebase.js'
+import { VueFire } from 'vuefire'
+import { firebaseApp } from './firebase.js'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { VueFireFirestoreOptionsAPI } from 'vuefire'
-import { VueFireAuthOptionsFromAuth } from 'vuefire'
+import { VueFireAuth } from 'vuefire'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(VueFire, {
-    firebaseApp, modules:[VueFireFirestoreOptionsAPI(), VueFireAuthOptionsFromAuth()]
-} )
+  firebaseApp,
+  modules: [VueFireFirestoreOptionsAPI(), VueFireAuth()],
+})
 
 app.mount('#app')
