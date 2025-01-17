5d9fe2b701bedd35eb982b3ef2608b0328968b15
kmujica <karina.mujica101@gmail.com>
2025-01-17 13:27:17 +0000

    fix tamaño logo en login para movil

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -21,7 +21,7 @@ export default {
         .then(() => {
           router.push('/')
         })
-        .catch((error) => {})
+        .catch((error) => { })
     },
   },
   mounted() {
@@ -29,26 +29,21 @@ export default {
     const uid = currentUser?.uid
     console.log('currentUser', uid)
   },
-  beforeMount() {},
+  beforeMount() { },
 }
 </script>
 
 <template>
   <div class="min-h-screen bg-white p-4 justify-center items-center">
-    <div
-      class="mx-auto max-w-2xl rounded-xl bg-white p-8 shadow-lg flex flex-col justify-center items-center"
-    >
-      <IconLogoPP class="h-30" />
+    <div class="mx-auto max-w-2xl rounded-xl bg-white p-8 shadow-lg flex flex-col justify-center items-center">
+      <IconLogoPP />
 
       <form class="flex flex-col justify-center items-center w-full py-6 gap-4" @submit.prevent>
         <div class="flex flex-col w-full items-center">
           <label class="block mb-1 justify-start" for="username">Correo electrónico</label>
           <input
             class="mt-1 block w-1/2 rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
-            type="text"
-            v-model="username"
-            placeholder="Correo electrónico"
-          />
+            type="text" v-model="username" placeholder="Correo electrónico" />
         </div>
 
         <div class="flex flex-col items-center w-full">
@@ -56,16 +51,10 @@ export default {
 
           <input
             class="mt-1 block w-1/2 rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
-            type="password"
-            v-model="password"
-            placeholder="Contraseña"
-          />
+            type="password" v-model="password" placeholder="Contraseña" />
         </div>
 
-        <button
-          class="m-auto w-1/2 rounded-md bg-blue-500 px-6 py-2 text-white hover:bg-blue-600"
-          @click="login"
-        >
+        <button class="m-auto w-1/2 rounded-md bg-blue-500 px-6 py-2 text-white hover:bg-blue-600" @click="login">
           Entrar
         </button>
       </form>
@@ -74,6 +63,5 @@ export default {
 </template>
 
 <style>
-.error {
-}
+.error {}
 </style>
