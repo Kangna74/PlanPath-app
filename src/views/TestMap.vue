00d8eadc9a966bf74c0b25b05ef994e2b1e3492e
JonathanBetPer <jonathanbetper@gmail.com>
2025-01-23 16:17:36 +0000

    Añadido libreria y vista básica

---
+++ src/views/TestMap.vue
@@ -0,0 +1,25 @@
+<template>
+  <MglMap :accessToken="accessToken" :mapStyle="mapStyle" />
+</template>
+
+<script>
+import Mapbox from "mapbox-gl";
+import { MglMap } from "vue-mapbox";
+
+export default {
+  components: {
+    MglMap
+  },
+  data() {
+    return {
+      accessToken: import.meta.env.VITE_MAPBOX_API_KEY, // your access token. Needed if you using Mapbox maps
+      mapStyle: MAP_STYLE // your map style
+    };
+  },
+
+  created() {
+    // We need to set mapbox-gl library here in order to use it in template
+    this.mapbox = Mapbox;
+  }
+};
+</script>
