120e540fb9a914415f8904faf9b2ee90083db412
CorvsX <alexxtrabajosvfx@gmail.com>
2025-01-28 19:12:30 +0000

    Footer

---
+++ src/components/SocialLink.vue
@@ -0,0 +1,36 @@
+<template>
+  <a :href="url" target="_blank" :aria-label="`Visítanos en ${name}`" class="text-2xl text-gray-800 hover:scale-110 hover:rotate-6 transition-transform duration-300 ease-in-out" :style="{ '--color-accent': colorAccent }">
+    <component :is="iconComponent" />
+  </a>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { LinkedinIcon, TwitterIcon, YoutubeIcon } from 'lucide-vue-next';
+
+const props = defineProps({
+  url: String,
+  name: String,
+  colorAccent: String
+});
+
+const iconComponent = computed(() => {
+  switch (props.name) {
+    case 'linkedin':
+      return LinkedinIcon;
+    case 'twitter':
+      return TwitterIcon;
+    case 'youtube':
+      return YoutubeIcon;
+    default:
+      return null;
+  }
+});
+</script>
+
+<style scoped>
+a:hover {
+  color: var(--color-accent);
+}
+</style>
+
